f15228f16d1a146047419a9f1d65aa236ea26bcb
mhaidar <mhaidar@areeba.com>
2025-10-04 10:52:17 +0300

    added CRUD operations to LoansApiClient interface with validation constraints

--- src/main/java/com/home/loans/apiclient/LoansApiClient.java
+++ src/main/java/com/home/loans/apiclient/LoansApiClient.java
@@ -1,7 +1,29 @@
 package com.home.loans.apiclient;
 
+import com.home.loans.dtos.LoanDTO;
+import com.home.loans.dtos.common.ResponseDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Validated
 public interface LoansApiClient {
+
+    ResponseEntity<ResponseDTO> createLoan(@RequestParam
+                                           @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                           String mobileNumber);
+
+    ResponseEntity<LoanDTO> fetchLoanDetails(@RequestParam
+                                             @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                             String mobileNumber);
+
+    ResponseEntity<ResponseDTO> updateLoanDetails(@Valid @RequestBody LoanDTO loanDTO);
+
+    ResponseEntity<ResponseDTO> deleteLoanDetails(@RequestParam
+                                                  @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                  String mobileNumber);
+
 }
