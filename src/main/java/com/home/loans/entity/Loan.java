6a2191747cf6529b1afada4ddf484bbf1559fa9e
mhaidar <mhaidar@areeba.com>
2025-12-06 19:23:54 +0200

    migrate to centralized configuration, refine profile setup, and integrate Spring Cloud Config. Fix entity and scripts.

--- src/main/java/com/home/loans/entity/Loan.java
+++ src/main/java/com/home/loans/entity/Loan.java
@@ -1,9 +1,6 @@
 package com.home.loans.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -12,6 +9,7 @@ import lombok.*;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "loans")
 public class Loan extends BaseEntity{
 
     @Id
